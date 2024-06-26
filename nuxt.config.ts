39c50d02240b60ec7c95e637b1b8be1abb2631f8
alyson.ramos <ramos.alysonfarias@gmail.com>
2024-06-26 00:36:11 -0300

    feat: disable ssr, add content, colorMode and sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: false },
+  ssr: false,
+  devtools: {
+    enabled: true,
+    timeline: {
+      enabled: true,
+    },
+  },
   future: {
     compatibilityVersion: 4,
+  },
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "@nuxt/content"],
+  content: {
+    highlight: {
+      theme: {
+        default: "min-light",
+        dark: "min-dark",
+      },
+    },
+  },
+  colorMode: {
+    classSuffix: "",
+  },
+  nitro: {
+    prerender: {
+      routes: ["/sitemap.xml"],
+    },
   },
 });
