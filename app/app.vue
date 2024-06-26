a6af110daa0b3db1de4bf90dc3428fc1c79081e5
alyson.ramos <ramos.alysonfarias@gmail.com>
2024-06-26 01:08:05 -0300

    refactor: app vue

--- app/app.vue
+++ app/app.vue
@@ -11,6 +11,19 @@
   </div>
 </template>
 
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.1s;
+}
+
+.page-enter-from,
+.page-leave-to {
+  opacity: 0.5;
+  filter: grayscale(1);
+}
+</style>
+
 <script setup lang="ts">
-const layout = "another";
+const layout = "default";
 </script>
