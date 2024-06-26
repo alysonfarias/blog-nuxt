aac5fbda9aae0447e20aba37f7a1daf7df3d4a5a
alyson.ramos <ramos.alysonfarias@gmail.com>
2024-06-26 01:09:17 -0300

    refactor: blog

--- app/layouts/another.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div>
-        <h1>Another Layout</h1>
-        <slot />
-    </div>
-</template>
--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,11 +1,5 @@
 <template>
-    <div>
-        about me
-    </div>
+    <article class="prose dark:prose-invert max-w-none">
+        <ContentDoc path="/about" />
+    </article>
 </template>
-
-<script setup lang="ts">
-definePageMeta({
-    layout: 'custom'
-})
-</script>
