a5287eb94f5d9b58f9f301497d6c6e87939e0938
alyson.ramos <ramos.alysonfarias@gmail.com>
2024-07-20 19:06:33 -0300

    feat: add og and seo

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Alyson Farias',
+  ogTitle: 'alyson farias',
+  description: 'alyson farias - digital garden',
+  ogDescription: 'alyson farias - digital garden',
+  ogImage: 'https://api.deepai.org/job-view-file/533d2fa2-e867-411f-8051-adb15c9218ec/outputs/output.jpg?art-image=true',
+  twitterCard: 'summary_large_image',
+})
+</script>
+
 <template>
   <article class="prose dark:prose-invert max-w-none">
     <ContentDoc path="/" />
