ed74174f66146733ad443e48c07e4dc3fdfdca29
alyson.ramos <ramos.alysonfarias@gmail.com>
2024-07-20 20:12:19 -0300

    feat: add data to latest post

--- app/components/content/latest-blog-posts.vue
+++ app/components/content/latest-blog-posts.vue
@@ -1,6 +1,7 @@
 <template>
   <BlogPosts v-slot="{ posts }" :limit="limit">
     <div v-for="post in posts" :key="post.id">
+      <span class="text-xs">{{ formatDate(post.publishedAt) }}</span> |
       <NuxtLink :to="post._path">{{ post.title }}</NuxtLink>
     </div>
   </BlogPosts>
@@ -13,4 +14,15 @@ defineProps({
     default: null
   }
 })
+
+const formatDate = (dateString, displayYear) => {
+  if (!dateString) return ''; // Handle missing dates
+
+  const date = new Date(dateString);
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+
+
+
+  return date.toLocaleDateString(undefined, options);
+};
 </script>
