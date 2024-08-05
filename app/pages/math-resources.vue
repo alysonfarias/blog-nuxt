45013108af80d6ccf35d138a847e19b736cf3629
alyson.ramos <ramos.alysonfarias@gmail.com>
2024-08-05 09:16:48 -0300

    fix: routes

--- app/pages/math-resources.vue
+++ app/pages/math-resources.vue
@@ -1,5 +1,5 @@
 <template>
     <article class="prose dark:prose-invert max-w-none">
-        <ContentDoc path="/awesome-talks" />
+        <ContentDoc path="/math-resources" />
     </article>
 </template>
