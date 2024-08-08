3405cdd9e46505a4a7fc773bce503b91cd1b532e
alyson.ramos <ramos.alysonfarias@gmail.com>
2024-08-08 15:29:22 -0300

    feat: age

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mx-auto max-w-4xl">
+  <div class="container mx-auto max-w-4xl flex flex-col min-h-screen">
     <header class="flex justify-between items-start md:items-center mt-5">
       <div class="flex items-center md:space-x-12">
         <div class="hidden md:block">
@@ -9,17 +9,20 @@
 
         <Menu />
       </div>
-
       <ClientOnly>
         <ColorModeSelector />
       </ClientOnly>
     </header>
 
-    <main class="p-2 mt-10">
+    <main class="p-2 mt-10 flex-grow">
       <slot />
     </main>
+    <footer class="mt-auto flex justify-end">
+      <small>{{ age }}y</small>
+    </footer>
   </div>
 </template>
+
 
 <script setup>
 useHead({
@@ -36,6 +39,29 @@ useHead({
   //   }
   // ]
 })
+import { onMounted, ref } from 'vue';
+
+const age = ref(null);
+
+function calculateAge() {
+  const birthYear = 2001;
+  const monthYear = 12;
+  const fictionalDayDate = 1
+  const birthDate = new Date(birthYear, monthYear, fictionalDayDate);
+
+  const now = new Date();
+  const ageInMilliseconds = now - birthDate;
+  const years = ageInMilliseconds / (1000 * 60 * 60 * 24 * 365.25);
+
+  return years.toFixed(10);
+}
+
+onMounted(() => {
+  setInterval(() => {
+    age.value = calculateAge();
+  }, 900);
+});
+
 </script>
 
 <style>
