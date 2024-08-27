07030df6353dc726dedd8885487bedda69089777
Alyson Farias <63425960+alysonfarias@users.noreply.github.com>
2024-08-27 11:43:53 -0300

    add seo into the app.vue

--- app/app.vue
+++ app/app.vue
@@ -15,3 +15,14 @@
   filter: grayscale(1);
 }
 </style>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Alyson Farias ',
+  ogTitle: 'alyson farias',
+  description: 'alyson farias - digital garden',
+  ogDescription: 'alyson farias - digital garden',
+  ogImage: 'https://api.deepai.org/job-view-file/533d2fa2-e867-411f-8051-adb15c9218ec/outputs/output.jpg?art-image=true',
+  twitterCard: 'summary_large_image',
+})
+</script>
